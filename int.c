d0a971596d0a2751b15d9dc2069f204136d104c1
steve7987 <steve.7987@gmail.com>
2014-08-25 10:20:19 +0900

    Added base files
    Added the files int.c int.h and main.c that had been written previously.

---
+++ int.c
@@ -0,0 +1,466 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "int.h"
+
+int SaveInt(char * filename, BigInt * tosave){
+	FILE * fp = fopen(filename, "wb");
+	int length = tosave->length;
+	while(tosave->num[length - 1] == 0){
+		length--;
+	}
+	if (length == 0){
+		length = 1;
+	}
+	if (!fp){
+		printf("unable to open file %s", filename);
+		return 1;
+	}
+	
+	if (fwrite(tosave->num, sizeof(unsigned char), length, fp) != length){
+		printf("Failed to write file %s", filename);
+		return 1;
+	}
+	fclose(fp);
+	return 0;
+}
+
+int LoadInt(char * filename, BigInt * result){
+	FILE * fp = fopen(filename, "rb");
+	size_t filesize;
+	if (!fp){
+		printf("Unable to open file %s", filename);
+		return 1;
+	}
+	fseek(fp, 0, SEEK_END);
+	filesize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	result->num = malloc(sizeof(unsigned char) * ((filesize + sizeof(unsigned char) - 1) / sizeof(unsigned char)));
+	if (!result->num){
+		printf("Malloc failed when loading file %s", filename);
+		return 1;
+	}
+	result->length = (filesize + sizeof(unsigned char) - 1) / sizeof(unsigned char);
+	result->num[result->length - 1] = 0;
+	result->negative = 0;  //positive number
+	if (fread(result->num, 1, filesize, fp) != filesize){
+		printf("Failed reading file %s", filename);
+		return 1;
+	}
+	
+	fclose(fp);
+	return 0;
+}
+
+int CopyInt(BigInt * source, BigInt * dest){
+	dest->num = malloc(source->length * sizeof(unsigned char));
+	if (!dest->num){
+		printf("Copy failed");
+		return 1;
+	}
+	int i;
+	dest->length = source->length;
+	for (i = 0; i < source->length; i++){
+		dest->num[i] = source->num[i];
+	}
+	dest->negative = source->negative;
+	return 0;
+}
+
+void DeleteInt(BigInt * deleteme){
+	if (deleteme->num){
+		free(deleteme->num);
+		deleteme->num = 0;
+	}
+}
+
+void PrintInt(BigInt * printme){
+	int i = printme->length - 1;
+	while (printme->num[i] == 0) i--;
+	if (printme->negative == 1){
+		printf("-");
+	}
+	for (; i >= 0; i--){
+		printf("%0*X  ", sizeof(unsigned char)*2, printme->num[i]);
+
+	}
+	printf("\n");
+}
+
+double Approximate(BigInt * x){
+	double ret = 0;
+	double pow = 1;
+	int i;
+	for (i = 0; i < x->length; i++){
+		ret += x->num[i] * pow;
+		pow = pow * (1 << 8*sizeof(unsigned char));
+	}
+	if (x->negative == 1){
+		ret = ret * -1;
+	}
+	return ret;
+}
+
+int Compare(BigInt * x, BigInt * y){
+	int i;
+	if (x->length > y->length){
+		//check if anything is non zero
+		for (i = x->length - 1; i >= (int) y->length; i--){
+			if(x->num[i] != 0){
+				return 1;
+			}
+		}
+	}
+	else if (x->length < y->length){
+		//check if anything is non zero
+		for (i = y->length - 1; i >= x->length; i--){
+			if(y->num[i] != 0) return -1;
+		}
+	}
+	else {
+		i = x->length;
+	}
+	for (; i >= 0; i--){
+		
+		if (x->num[i] > y->num[i]) return 1;
+		if (x->num[i] < y->num[i]) return -1;
+	}
+	return 0;
+}
+
+void SubtractS(BigInt * x, unsigned char y){
+	//check if x > y
+	
+	
+	//subtract y
+	int i = 0;
+	while (y != 0){
+		if (y <= x->num[i]) {
+			x->num[i] = x->num[i] - y;
+			return;
+		}
+		//need to borrow
+		x->num[i] = (1 << 8*sizeof(unsigned char)) + x->num[i] - y;
+		y = 1;
+		i++;
+	}
+}
+
+void DivideS(BigInt * x, unsigned char y){
+	if (y == 0 || y == 1) return;
+	int overflow = 0;
+	int quotient = 0;
+	int i;
+	for (i = x->length - 1; i >= 0; i--){
+		quotient = (x->num[i] + (overflow << (8*sizeof(unsigned char)))) / y;
+		overflow = (x->num[i] + (overflow << (8*sizeof(unsigned char)))) - quotient * y;
+		x->num[i] = quotient;
+	}
+}
+
+unsigned char ModuloS(BigInt * x, unsigned char y){
+	unsigned char retval = 0;
+	int factor = 1;
+	int i;
+	for (i = 0; i < x->length; i++){
+		retval = (retval + x->num[i]*factor) % y;
+		factor = (factor << sizeof(unsigned char) * 8) % y;
+	}
+	return retval;
+}
+
+void LeftShift(BigInt * x, unsigned int amt){
+	//shift by mults here
+	
+	//shift by rest
+	int enter = 0;
+	int exit = 0;
+	int i;
+	for (i = 0; i < x->length; i++){
+		exit = x->num[i] >> (8*sizeof(unsigned char) - (amt % (8*sizeof(unsigned char))));
+		x->num[i] = (x->num[i] << (amt % (8*sizeof(unsigned char)))) | enter;
+		enter = exit;
+	}
+	if (exit != 0){
+		//need to expand int
+		unsigned char * tmp = malloc(sizeof(unsigned char)*2*x->length);
+		for (i = 0; i < x->length; i++){
+			tmp[i] = x->num[i];
+		}
+		for (i = x->length; i < 2*x->length; i++){
+			tmp[i] = 0;
+		}
+		tmp[x->length] = exit;
+		x->length = 2*x->length;
+		free(x->num);
+		x->num = tmp;
+	}
+}
+
+int Add(BigInt * x, BigInt * y, BigInt * sum){
+	//make y the smaller int based of length only
+	if (y->length > x->length){
+		BigInt * tmp = x;
+		x = y;
+		y = tmp;
+	}
+	sum->length = x->length + 1;
+	sum->negative = 0;
+	sum->num = malloc(sum->length * sizeof(char));
+	if (!sum->num){
+		printf("Malloc failed when adding");
+		return 1;
+	}
+	int curindex;
+	int carry = 0;
+	//add x and y with carry
+	for (curindex = 0; curindex < y->length; curindex++){
+		carry = carry + x->num[curindex] + y->num[curindex];
+		sum->num[curindex] = (unsigned char) carry;
+		carry = carry >> 8*sizeof(unsigned char);
+	}
+	//y out of digits so just add x and carry
+	for (; curindex < x->length; curindex++){
+		carry = carry + x->num[curindex];
+		sum->num[curindex] = (unsigned char) carry;
+		carry = carry >> 8*sizeof(unsigned char);
+	}
+	sum->num[curindex] = (unsigned char) carry;
+	return 0;
+}
+
+int Subtract(BigInt * x, BigInt * y, BigInt * difference){
+	//assuming both are positive
+	if (Compare(x, y) < 0){
+		BigInt * tmp = x;
+		x = y;
+		y = tmp;
+		difference->negative = 1;
+	}
+	else {
+		difference->negative = 0;
+	}
+	
+	//assume x >= y
+	difference->num = malloc(sizeof(unsigned char)*x->length);
+	if (!difference->num){
+		printf("Error with malloc in Subtract");
+		return 1;
+	}
+	difference->length = x->length;
+	int borrow = 0;
+	int i;
+	//subtracting y from x with borrowing
+	for (i = 0; i < y->length; i++){
+		if (x->num[i] >= y->num[i] + borrow){
+			difference->num[i] = x->num[i] - y->num[i] - borrow;
+			borrow = 0;
+		}
+		else {
+			difference->num[i] = x->num[i] - y->num[i] - borrow + (1 << (8*sizeof(unsigned char)));
+			borrow = 1;
+		}
+	}
+	//y out of digits just deal with borrow
+	while (borrow != 0){
+		if (x->num[i] >= borrow){
+			difference->num[i] = x->num[i] - borrow;
+			borrow = 0;
+		}
+		else {
+			difference->num[i] = x->num[i] - y->num[i] + (1 << (8*sizeof(unsigned char)));
+			borrow = 1;
+		}
+		i++;
+	}
+	//copy down extra digits of x
+	for (;i < x->length; i++){
+		difference->num[i] = x->num[i];
+	}
+	for (;i < difference->length; i++){
+		difference->num[i] = 0;
+	}
+}
+
+int Multiply(BigInt * x, BigInt * y, BigInt * product){
+	product->length = x->length + y->length;
+	product->num = malloc(sizeof(unsigned char)*product->length);
+	if (!product->num){
+		printf("Malloc in multiplication failed");
+		return 1;
+	}
+	int i, j;
+	int carry = 0;
+	int sum;
+	for (i = 0; i < product->length; i++){  //loop over digits of result
+		sum = carry;
+		for (j = 0; j <= i; j++){  //add in all combos that give digit i
+			if (j < x->length && i-j < y->length){  
+				sum = sum + x->num[j]*y->num[i-j];
+			}
+		}
+		product->num[i] = (unsigned char) sum;
+		carry = sum >> (8*sizeof(unsigned char));
+	}
+	product->negative = x->negative ^ y->negative;
+	return 0;
+}
+
+//basically subtract but does x = x - y;
+int DivideHelp(BigInt * x, BigInt * y){
+	int borrow = 0;
+	int i;
+	for (i = 0; i < y->length; i++){
+		if (x->num[i] >= y->num[i] + borrow){
+			x->num[i] = x->num[i] - y->num[i] - borrow;
+			borrow = 0;
+		}
+		else {
+			x->num[i] = x->num[i] - y->num[i] - borrow + (1 << (8*sizeof(unsigned char)));
+			borrow = 1;
+		}
+	}
+	while (borrow != 0){
+		if (x->num[i] >= borrow){
+			x->num[i] = x->num[i] - borrow;
+			borrow = 0;
+		}
+		else {
+			x->num[i] = x->num[i] - y->num[i] + (1 << (8*sizeof(unsigned char)));
+			borrow = 1;
+		}
+		i++;
+	}
+}
+
+int Divide(BigInt * x, BigInt * y, BigInt * quotient){
+	//proceed assuming y smaller than x
+	BigInt remainder;
+	remainder.length = y->length + 1;
+	remainder.num = malloc(sizeof(unsigned char)*remainder.length);
+	quotient->length = x->length - y->length + 1;
+	quotient->num = malloc(sizeof(unsigned char)*quotient->length);
+	if (!quotient->num){
+		printf("Malloc division failed");
+		return 1;
+	}
+	//set remainder and quotient to zero
+	int i;
+	for (i = 0; i < remainder.length; i++){
+		remainder.num[i] = 0;
+	}
+	for (i = 0; i < quotient->length; i++){
+		quotient->num[i] = 0;
+	}
+	//follow algorithm for binary division
+	for (i = x->length*sizeof(unsigned char)*8 - 1; i >= 0; i--){
+		LeftShift(&remainder, 1);
+		remainder.num[0] = remainder.num[0] | (x->num[i / (8*sizeof(unsigned char))] >> (i%(8*sizeof(unsigned char))));
+		if (Compare(&remainder, y) >= 0){
+			DivideHelp(&remainder, y);
+			quotient->num[i / (8*sizeof(unsigned char))] = quotient->num[i / (8*sizeof(unsigned char))] | (1 << (i%(8*sizeof(unsigned char))));
+		}
+	}
+	//dont need remainder here
+	free(remainder.num);
+	quotient->negative = x->negative ^ y->negative;
+	return 0;
+}
+
+int PrintBase10(BigInt * x){
+	char * ret;
+	int rlength;
+	int index;
+	BigInt p, zero;
+	CopyInt(x, &p);
+	zero.length = 0;
+	ret = malloc(x->length*sizeof(char));  //can expand if necessary
+	if (!ret){
+		printf("Malloc failed in base 10 method");
+		return 1;
+	}
+	rlength = x->length;
+	index = 0;
+	while (Compare(&p, &zero) > 0){
+		ret[index] = '0' + ModuloS(&p, 10);
+		DivideS(&p, 10);
+		index++;
+		//if out of space make array bigger
+		if (index >= rlength){
+			int i;
+			char * tmp = malloc(2*rlength*sizeof(char));
+			if (!tmp){
+				printf("Malloc failed in base 10 method");
+				return 1;
+			}
+			for (i = 0; i < rlength; i++){
+				tmp[i] = ret[i];
+			}
+			free(ret);
+			ret = tmp;
+			rlength = 2*rlength;
+		}
+	}
+	//reverse array
+	int i;
+	for (i = 0; i < index/2; i++){
+		char t = ret[i];
+		ret[i] = ret[index-i-1];
+		ret[index-i-1] = t;
+	}
+	ret[index] = '\0';
+	DeleteInt(&p);
+	if (x->negative == 1){
+		printf("-");
+	}
+	printf(ret);
+	printf("\n");
+	free(ret);
+	return 0;
+}
+
+char * ToBase10(BigInt * x){
+	char * ret;
+	int rlength;
+	int index;
+	int offset = x->negative;  //need extra char for negative number
+	BigInt p, zero;
+	CopyInt(x, &p);
+	zero.length = 0;
+	ret = malloc(x->length*sizeof(char));  //can expand if necessary
+	if (!ret){
+		printf("Malloc failed in base 10 method");
+		return 0;
+	}
+	rlength = x->length;
+	index = 0;
+	while (Compare(&p, &zero) > 0){
+		ret[index] = '0' + ModuloS(&p, 10);
+		DivideS(&p, 10);
+		index++;
+		//if out of space make array bigger
+		if (index + offset >= rlength){
+			int i;
+			char * tmp = malloc(2*rlength*sizeof(char));
+			if (!tmp){
+				printf("Malloc failed in base 10 method");
+				return 0;
+			}
+			for (i = 0; i < rlength; i++){
+				tmp[i] = ret[i];
+			}
+			free(ret);
+			ret = tmp;
+			rlength = 2*rlength;
+		}
+	}
+	//reverse array
+	int i;
+	for (i = 0; i < index/2; i++){
+		char t = ret[i];
+		ret[i] = ret[index-i-1];
+		ret[index-i-1] = t;
+	}
+	ret[index] = '\0';
+	DeleteInt(&p);
+	return ret;
+}
---
+++ int.h
@@ -0,0 +1,42 @@
+//num[0] is the first digit, and num[length - 1] the last
+typedef struct
+{
+	size_t length;  
+	unsigned char * num;
+	int negative;  //0 is positive, 1 is negative
+} BigInt;
+
+//in general these functions return 1 if the function failed, and 0 if it succeeded
+//when saving and loading the least significant byte is stored first
+int LoadInt(char * filename, BigInt * result);  //loads a big int from a file into memory
+int SaveInt(char * filename, BigInt * tosave);  //saves a big int to a file
+int CopyInt(BigInt * source, BigInt * dest);  //makes a deep copy of the source
+void DeleteInt(BigInt * deleteme);  //frees the data of the int
+void PrintInt(BigInt * printme);  //prints the int in hexadecimal
+int Compare(BigInt * x, BigInt * y);  //returns 1 if x > y, 0 if x == y, -1 if x < y
+double Approximate(BigInt * x);  //gives the double approximation of this int
+
+
+//these functions perform arithmetic where one input is small
+//in general they will modify the input big int (except modulo)
+void AddS(BigInt * x, unsigned char y);
+void SubtractS(BigInt * x, unsigned char y);
+void MultiplyS(BigInt * x, unsigned char y);
+void DivideS(BigInt * x, unsigned char y);
+unsigned char ModuloS(BigInt * x, unsigned char y);
+void RightShift(BigInt * x, unsigned int amt);  //right shifts x by amt bits  
+void LeftShift(BigInt * x, unsigned int amt);  //left shifts x by amt bits (excess bits should not be discarded)
+
+
+//these functions perform arithmetic where both inputs are big
+//they should not modify either input
+//in general they return 1 if the function failed, and 0 if it succeeded
+int Add(BigInt * x, BigInt * y, BigInt * sum);
+int Subtract(BigInt * x, BigInt * y, BigInt * difference);
+int Multiply(BigInt * x, BigInt * y, BigInt * product);
+int Divide(BigInt * x, BigInt * y, BigInt * quotient);
+int Modulo(BigInt * x, BigInt * y, BigInt * remainder);
+
+char * ToBase10(BigInt * x);  //returns a null terminated string that contains the base 10 representation of the int
+void GCD(BigInt * x, BigInt * y, BigInt * gcd);  //computes the greatest common divisor of the two inputs
+
